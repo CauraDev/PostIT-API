567c542e321ed5feaff991dbb0e86519f88ccfe7
unknown <coria.tomasg@gmail.com>
2021-12-31 03:11:59 +0100

    Loader and fetch scripts done, right before limit Selector scrips

--- index.js
+++ index.js
@@ -1,1 +1,72 @@
-console.log("Tamo");
+const postContainer = document.querySelector(".postContainer");
+const loader = document.querySelector(".loader");
+const inputFilter = document.querySelector("#filter")
+
+//https://jsonplaceholder.typicode.com/posts?_page=1&_limit=5
+
+let page = 1;
+let limit = 1;
+
+const getPosts = async () => {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=${limit}`)
+
+    const data = await response.json();
+
+    return data;
+}
+
+const printPosts = (postsToPrint) => {
+
+    const postsHTML = postsToPrint.map((value) => {
+
+        return `<div class="post">
+                    <div class="postNumber">${value.id}</div>
+                    <div class="postBody">
+                        <h2 class="postTitle">${value.title}</h2>
+                         <p class="postText">${value.body}</p>
+                    </div>
+                </div>`
+    
+    }).join("");
+
+    postContainer.innerHTML += postsHTML;
+}
+
+function initiate() {
+    window.addEventListener("DOMContentLoaded", async () => {
+        let posts = await getPosts();
+        printPosts(posts);
+    });
+
+
+    window.addEventListener("scroll", async () => {
+
+        
+        const {scrollTop, clientHeight, scrollHeight} = document.documentElement;
+
+        if (scrollTop + clientHeight >= scrollHeight) {
+
+            page++;
+           
+            const newPosts = await getPosts();
+
+            showLoader(newPosts);
+        }
+    })
+}
+
+const showLoader = (newPostsReceived) => {
+    loader.style.opacity = 1;
+
+    const randomNumber = Math.floor(Math.random() * 3000);
+
+    setTimeout(() => {
+        loader.style.opacity = 0;
+
+        setTimeout(() => {
+            printPosts(newPostsReceived)
+        }, 100);
+    }, randomNumber);
+};
+
+initiate();
